6ca8aa1db619f91e81db853c1614ef15e7e39efb
Jer Bartels <jer@iamjer.be>
2016-03-26 11:18:15 +0100

    basisstructuur project opzetten

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once("classes/Comment.class.php");
+include_once("classes/Post.class.php");
+include_once("classes/User.class.php");
+include_once("classes/Db.class.php");
+
+?>
+
+<!doctype html>
+
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Satchmo</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles/reset.css">
+        <link rel="stylesheet" href="styles/style.css">
+    </head>
+
+    <body>
+
+    </body>
+
+</html>
